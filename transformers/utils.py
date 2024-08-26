f4eb07455e7b5f3e0294644d3081c4f60557c3d8
Ohlindoh <olindo.verrillo@gmail.com>
2024-08-26 02:18:51 +0000

    Adding updated, functional week tracker

--- transformers/utils.py
+++ transformers/utils.py
@@ -1,16 +1,24 @@
 import requests
 import logging
+import datetime
 
 logger = logging.getLogger(__name__)
 
 ESPN_API_URL = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard'
 
 def get_current_nfl_week():
-    """Fetch the current NFL week number from ESPN API."""
+    """Fetch the current NFL week number from ESPN API based on the current date."""
     response = requests.get(ESPN_API_URL)
     if response.status_code == 200:
         data = response.json()
-        return data.get('week', {}).get('number')
+        current_date = datetime.datetime.utcnow()
+        season_info = data.get('season', {}).get('entries', [])
+        for week in season_info:
+            start = datetime.datetime.fromisoformat(week['startDate'].replace('Z', '+00:00'))
+            end = datetime.datetime.fromisoformat(week['endDate'].replace('Z', '+00:00'))
+            if start <= current_date <= end:
+                return week['value']
+        return '1'  # Default to week 1 if current date is not within any week range
     logger.error(f"Failed to retrieve data: {response.status_code}")
     return None
 
